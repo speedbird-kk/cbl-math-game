a74dc5546463eb99012939708d77b849937ddad0
Bogdan Mladin <bogdan.mladin12@gmail.com>
2025-10-24 22:01:23 +0200

    Removed GameContext from title screen

--- src/TitleScreenPanel.java
+++ src/TitleScreenPanel.java
@@ -11,15 +11,12 @@ import Experimentation.view.styles.ApplyStyles;
 import Experimentation.view.styles.DimensionConstants;
 
 public class TitleScreenPanel extends JPanel {
-    GameContext gameContext;
-
     TitleButton titleButton;
 
-    TitleScreenPanel(GameContext gameContext) {
-        this.gameContext = gameContext;
+    TitleScreenPanel() {
         ApplyStyles.TITLE_SCREEN_STYLE.on(this);
         this.setLayout(null);
-        titleButton = new TitleButton(gameContext);
+        titleButton = new TitleButton();
         this.add(titleButton);
     }
     public void timeUpdate(int t) {
@@ -27,17 +24,16 @@ public class TitleScreenPanel extends JPanel {
     }
 }
 class TitleButton extends JButton {
-    GameContext gameContext;
     long animationTime = 0;
     boolean isPressed = false;
     int tileWidth = DimensionConstants.GRID.get().width;
     int tileHeight = DimensionConstants.GRID.get().height;
 
-    TitleButton(GameContext gameContext) {
-        this.gameContext = gameContext;
-        this.setBounds(tileWidth * 6, tileHeight * 4, DimensionConstants.TITLE_BUTTON.get().width,
+    TitleButton() {
+        this.setBounds(tileWidth * 6, tileHeight * 0, DimensionConstants.TITLE_BUTTON.get().width,
                 DimensionConstants.TITLE_BUTTON.get().height);
         this.setFocusable(false);
+        this.setText("Play");
         this.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -53,7 +49,9 @@ class TitleButton extends JButton {
         int newWidth = tileWidth * 4 + (int) (tileWidth / 2 * sinT);
         int newHeight = tileHeight + (int) (tileHeight / 4 * sinT);
         int newX = 16 * tileWidth / 2 - newWidth / 2;
-        int newY = 9 * tileHeight / 2 - newHeight / 2;
+        int newY = 5 * tileHeight / 2 - newHeight / 2;
+        
+        this.setFont(this.getFont().deriveFont((float)(newHeight*0.5)));
 
         this.setBounds(newX, newY, newWidth, newHeight);
     }
